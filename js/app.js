dba6254ba1e72f5a7790d95620ea828c7d1ba06c
Diana <dianachcastro@yahoo.es>
2017-11-07 01:18:47 -0300

    subiendo boton tweet

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var contenido_textarea = ""
 var num_caracteres_permitidos = 140 
 
 function valida_longitud(){ 
+	//se define extensión de caracteres por longitud
    num_caracteres = document.forms[0].texto.value.length 
 
    if (num_caracteres > num_caracteres_permitidos){ 
@@ -13,6 +14,7 @@ function valida_longitud(){
    } 
 
    if (num_caracteres >= num_caracteres_permitidos){ 
+   	//al llegar a 140 caracteres, tomar letra color rojo
       document.forms[0].caracteres.style.color="#ff0000"; 
    }else{ 
       document.forms[0].caracteres.style.color="#000000"; 
@@ -23,3 +25,18 @@ function valida_longitud(){
 function cuenta(){ 
    document.forms[0].caracteres.value=document.forms[0].texto.value.length 
 } 
+
+// Botón para twitear
+function operacion(){
+if(x==0){
+  //solo entra aquí cuando x sea cero
+  frames.mi_iframe.document.getElementById("textarea").value=document.getElementById("texto").value;
+  x++; 
+// variable cambia a uno despues de copiar el texto.
+} else {
+//solo entra aquí cuando x sea uno
+  document.formulario.submit(); 
+//aquí envia el form.
+}
+}
+
