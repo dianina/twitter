56da82d0e2b146fb1c584dfe52dcf1d700691da6
Diana <dianachcastro@yahoo.es>
2017-11-11 17:54:09 -0300

    subiendo contador retro

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
-//Formulario contador
+//-------------Formulario contador
+        function count_down(obj) {
+             
+            var element = document.getElementById('count2');
+             
+            element.innerHTML = 140 - obj.value.length;
+             
+            if (140 - obj.value.length < 140) {
+                element.style.color = 'red';
+            
+            } else {
+                element.style.color = 'grey';
+            }
+        }
+
+  function add_comments(){
+  //tomar comentarios
+    var comentarios = document.getElementById('comentario').value;
+  //limpiar....
+  document.getElementById('comment').value = '';
+
+  //contenedor que esta en el html
+  var cont_comm = document.getElementById('cont_comm');
+
+  //creamos el div que contiene cada comentario
+  var newComments = document.createElement('div');
+
+  //validar que textarea tenga comentarios
+  if(comments.length == 0 || comments == null) {
+    alert('Por favor ingresar un mensaje');
+    return false;
+  }
+
+
+/*-------------adicionales de los caracteres
 
 var contenido_textarea = "" 
 var num_caracteres_permitidos = 140 
@@ -14,7 +48,7 @@ function valida_longitud(){
    } 
 
    if (num_caracteres >= num_caracteres_permitidos){ 
-   	//al llegar a 140 caracteres, tomar letra color rojo
+   	///al llegar a 140 caracteres, tomar letra color rojo
       document.forms[0].caracteres.style.color="#ff0000"; 
    }else{ 
       document.forms[0].caracteres.style.color="#000000"; 
@@ -22,21 +56,11 @@ function valida_longitud(){
 
    cuenta() 
 } 
+
+//funcion contador
+
 function cuenta(){ 
    document.forms[0].caracteres.value=document.forms[0].texto.value.length 
 } 
 
-// Botón para twitear
-function operacion(){
-if(x==0){
-  //solo entra aquí cuando x sea cero
-  frames.mi_iframe.document.getElementById("textarea").value=document.getElementById("texto").value;
-  x++; 
-// variable cambia a uno despues de copiar el texto.
-} else {
-//solo entra aquí cuando x sea uno
-  document.formulario.submit(); 
-//aquí envia el form.
-}
-}
-
+//Boton para insertar commentarios*/
