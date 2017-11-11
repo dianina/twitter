83c9a6a2d54f80fb922b9f2f42ecbb43ea9200eb
Diana <dianachcastro@yahoo.es>
2017-11-11 19:51:09 -0300

    resguardando contador

--- js/app.js
+++ js/app.js
@@ -1,54 +1,20 @@
-//-------------Formulario contador
-        function count_down(obj) {
-             
-            var element = document.getElementById('count2');
-             
-            element.innerHTML = 140 - obj.value.length;
-             
-            if (140 - obj.value.length < 140) {
-                element.style.color = 'red';
-            
-            } else {
-                element.style.color = 'grey';
-            }
-        }
-
-  function add_comments(){
-  //tomar comentarios
-    var comentarios = document.getElementById('comentario').value;
-  //limpiar....
-  document.getElementById('comment').value = '';
-
-  //contenedor que esta en el html
-  var cont_comm = document.getElementById('cont_comm');
-
-  //creamos el div que contiene cada comentario
-  var newComments = document.createElement('div');
-
-  //validar que textarea tenga comentarios
-  if(comments.length == 0 || comments == null) {
-    alert('Por favor ingresar un mensaje');
-    return false;
-  }
-
-
-/*-------------adicionales de los caracteres
+ //Formulario contador
 
 var contenido_textarea = "" 
 var num_caracteres_permitidos = 140 
 
 function valida_longitud(){ 
-	//se define extensión de caracteres por longitud
+   //se define extensión de caracteres por longitud
    num_caracteres = document.forms[0].texto.value.length 
 
    if (num_caracteres > num_caracteres_permitidos){ 
       document.forms[0].texto.value = contenido_textarea 
    }else{ 
-      contenido_textarea = document.forms[0].texto.value	
+      contenido_textarea = document.forms[0].texto.value 
    } 
 
    if (num_caracteres >= num_caracteres_permitidos){ 
-   	///al llegar a 140 caracteres, tomar letra color rojo
+      //al llegar a 140 caracteres, tomar letra color rojo
       document.forms[0].caracteres.style.color="#ff0000"; 
    }else{ 
       document.forms[0].caracteres.style.color="#000000"; 
@@ -56,11 +22,22 @@ function valida_longitud(){
 
    cuenta() 
 } 
-
-//funcion contador
-
 function cuenta(){ 
    document.forms[0].caracteres.value=document.forms[0].texto.value.length 
-} 
+}
 
-//Boton para insertar commentarios*/
+
+
+// Botón para twitear
+//function operacion(){
+//if(x==0){
+  //solo entra aquí cuando x sea cero
+  //frames.mi_iframe.document.getElementById("textarea").value=document.getElementById("texto").value;
+  //x++; 
+// variable cambia a uno despues de copiar el texto.
+//} else {
+//solo entra aquí cuando x sea uno
+  //document.formulario.submit(); 
+//aquí envia el form.
+//}
+//}
